3c2773a011fc8af4b85c78e5ad166cf6d79e7f3f
elljayuk <elljayua@gmail.com>
2026-04-15 18:39:34 +0100

    Add initial hello.py program

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("💖 Welcome 💖")
+print("My name is Liuba and this is my first GitHub commit!")
+print()
+answer = input("Do I really love coding? (yes/no): ").lower()
+if answer == "yes":
+    print("💖 Yes... it’s true. I do love coding 💖")
+elif answer == "no":
+    print("💔 That’s false... deep down, I really do 💖")
+else:
+    print("🤔 Hmm... I’ll take that as a maybe, but I’m still learning!")
