a7537f0b4437ff288ed52f623ea14813b308d408
elljayuk <elljayua@gmail.com>
2026-04-16 01:34:45 +0100

    Refactor hello.py by moving logic into a function for better structure

--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
-print("💖 Welcome 💖")
-print("My name is Liuba and this is my first GitHub commit!")
-print()
-answer = input("Do I really love coding? (yes/no): ").lower()
-if answer == "yes":
-    print("💖 Yes... it’s true. I do love coding 💖")
-elif answer == "no":
-    print("💔 That’s false... deep down, I really do 💖")
-else:
-    print("🤔 Hmm... I’ll take that as a maybe, but I’m still learning!")
+def ask_question():
+    print("💖 Welcome 💖")
+    print("My name is Liuba and this is my first GitHub commit!")
+    print()
+
+    answer = input("Do I really love coding? (yes/no): ").lower()
+
+    if answer == "yes":
+        print("💖 Yes... it’s true. I do love coding 💖")
+    elif answer == "no":
+        print("💔 That’s false... deep down, I really do 💖")
+    else:
+        print("🤔 Hmm... I’ll take that as a maybe, but I’m still learning!")
+
+
+ask_question()
